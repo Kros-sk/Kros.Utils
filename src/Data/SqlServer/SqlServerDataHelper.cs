f6ec5545285b99397f88a2dc7f310bad8ee67b92
Stano Peťko <stano@stanopetko.eu>
2019-12-12 12:25:52 +0100

    Reverted back ClientId

--- src/Data/SqlServer/SqlServerDataHelper.cs
+++ src/Data/SqlServer/SqlServerDataHelper.cs
@@ -9,6 +9,6 @@
         /// Identification of Microsoft SQL Server classes (used for example in <see cref="SqlServerIdGeneratorFactory"/>,
         /// <see cref="BulkActions.SqlServer.SqlServerBulkActionFactory"/>).
         /// </summary>
-        public const string ClientId = "Microsoft.Data.SqlClient";
+        public const string ClientId = "System.Data.SqlClient";
     }
 }
