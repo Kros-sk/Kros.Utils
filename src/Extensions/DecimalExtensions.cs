1b49a2ccbacf89c69c14824c82dc1e903d434d1f
Michal Aujeský <aujesky@kros.sk>
2020-02-03 14:01:02 +0100

    added decimal extension for round

---
+++ src/Extensions/DecimalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kros.Extensions
+{
+    /// <summary>
+    /// Extension methods for decimal <see cref="decimal"/>.
+    /// </summary>
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// Returns rounded decimal value.
+        /// When a number is halfway between two others, it is rounded toward the nearest
+        /// number that is away from zero.
+        /// </summary>
+        /// <param name="value">A decimal number to be rounded.</param>
+        /// <param name="decimalPlaces">The number of decimal places in the return value.</param>
+        /// <returns>Decimal.</returns>
+        public static decimal Round(this decimal value, int decimalPlaces)
+            => Math.Round(value, decimalPlaces, MidpointRounding.AwayFromZero);
+    }
+}
