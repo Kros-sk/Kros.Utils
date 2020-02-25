a0d4b397755fe1e7f76b02f55a10a4eba08a29c9
Stano Peťko <stano@stanopetko.eu>
2020-02-25 21:05:00 +0100

    Some changes in doc comments and tests

--- src/Extensions/NumberExtensions.cs
+++ src/Extensions/NumberExtensions.cs
@@ -8,9 +8,23 @@ namespace Kros.Extensions
     public static class NumberExtensions
     {
         /// <summary>
-        /// Returns rounded decimal value.
-        /// When a number is halfway between two others, it is rounded toward the nearest
-        /// number that is away from zero.
+        /// Rounds a decimal value to the nearest integer.
+        /// Numbers in midway between two numbers are rounded
+        /// using <see cref="MidpointRounding">MidpointRounding.AwayFromZero</see> method.
+        /// This is the difference from <c>Math.Round</c>, where the default method is
+        /// <see cref="MidpointRounding">MidpointRounding.ToEven</see>.
+        /// </summary>
+        /// <param name="d">A decimal number to be rounded.</param>
+        /// <returns>Rounded decimal number.</returns>
+        public static decimal Round(this decimal d)
+            => Math.Round(d, MidpointRounding.AwayFromZero);
+
+        /// <summary>
+        /// Rounds a decimal value to a specified number of fractional digits.
+        /// Numbers in midway between two numbers are rounded
+        /// using <see cref="MidpointRounding">MidpointRounding.AwayFromZero</see> method.
+        /// This is the difference from <c>Math.Round</c>, where the default method is
+        /// <see cref="MidpointRounding">MidpointRounding.ToEven</see>.
         /// </summary>
         /// <param name="d">A decimal number to be rounded.</param>
         /// <param name="decimals">The number of decimal places in the return value.</param>
@@ -19,74 +33,74 @@ namespace Kros.Extensions
             => Math.Round(d, decimals, MidpointRounding.AwayFromZero);
 
         /// <summary>
-        /// Returns rounded decimal value.
-        /// </summary>
-        /// <param name="d">A decimal number to be rounded.</param>
-        /// <param name="decimals">The number of decimal places in the return value.</param>
-        /// <param name="mode">Specification for how to round d if it is midway between two other numbers.</param>
-        /// <returns>Rounded decimal number.</returns>
-        public static decimal Round(this decimal d, int decimals, MidpointRounding mode)
-            => Math.Round(d, decimals, mode);
-
-        /// <summary>
-        /// Returns rounded decimal value.
-        /// When a number is halfway between two others, it is rounded toward the nearest
-        /// number that is away from zero.
-        /// </summary>
-        /// <param name="d">A decimal number to be rounded.</param>
-        /// <returns>Rounded decimal number.</returns>
-        public static decimal Round(this decimal d)
-            => Round(d, MidpointRounding.AwayFromZero);
-
-        /// <summary>
-        /// Rounds a decimal value to the nearest integer. A parameter specifies how to round
+        /// Rounds a decimal value to the nearest integer. A <paramref name="mode"/> specifies how to round
         /// the value if it is midway between two numbers.
         /// </summary>
         /// <param name="d">A decimal number to be rounded.</param>
-        /// <param name="mode">Specification for how to round d if it is midway between two other numbers.</param>
+        /// <param name="mode">Specification for how to round <paramref name="d"/> if it is midway between two other numbers.
+        /// </param>
         /// <returns>Rounded decimal number.</returns>
         public static decimal Round(this decimal d, MidpointRounding mode)
             => Math.Round(d, mode);
 
         /// <summary>
-        /// Returns rounded double value.
-        /// When a number is halfway between two others, it is rounded toward the nearest
-        /// number that is away from zero.
+        /// Rounds a decimal value to the nearest integer. A <paramref name="mode"/> specifies how to round
+        /// the value if it is midway between two numbers.
         /// </summary>
         /// <param name="d">A decimal number to be rounded.</param>
-        /// <param name="digits">The number of decimal places in the return value.</param>
-        /// <returns>Rounded double number.</returns>
-        public static double Round(this double d, int digits)
-            => Math.Round(d, digits, MidpointRounding.AwayFromZero);
+        /// <param name="decimals">The number of decimal places in the return value.</param>
+        /// <param name="mode">Specification for how to round <paramref name="d"/> if it is midway between two other numbers.
+        /// </param>
+        /// <returns>Rounded decimal number.</returns>
+        public static decimal Round(this decimal d, int decimals, MidpointRounding mode)
+            => Math.Round(d, decimals, mode);
 
         /// <summary>
-        /// Returns rounded double value.
-        /// </summary>
-        /// <param name="d">A double number to be rounded.</param>
-        /// <param name="digits">The number of decimal places in the return value.</param>
-        /// <param name="mode">Specification for how to round d if it is midway between two other numbers.</param>
-        /// <returns>Rounded double number.</returns>
-        public static double Round(this double d, int digits, MidpointRounding mode)
-            => Math.Round(d, digits, mode);
-
-        /// <summary>
-        /// Returns rounded double value.
-        /// When a number is halfway between two others, it is rounded toward the nearest
-        /// number that is away from zero.
+        /// Rounds a decimal value to the nearest integer.
+        /// Numbers in midway between two numbers are rounded
+        /// using <see cref="MidpointRounding">MidpointRounding.AwayFromZero</see> method.
+        /// This is the difference from <c>Math.Round</c>, where the default method is
+        /// <see cref="MidpointRounding">MidpointRounding.ToEven</see>.
         /// </summary>
         /// <param name="d">A decimal number to be rounded.</param>
-        /// <returns>Rounded double number.</returns>
+        /// <returns>Rounded decimal number.</returns>
         public static double Round(this double d)
-            => Round(d, MidpointRounding.AwayFromZero);
+            => Math.Round(d, MidpointRounding.AwayFromZero);
 
         /// <summary>
-        /// Rounds a double-precision floating-point value to the nearest integer. A parameter
-        /// specifies how to round the value if it is midway between two numbers.
+        /// Rounds a decimal value to a specified number of fractional digits.
+        /// Numbers in midway between two numbers are rounded
+        /// using <see cref="MidpointRounding">MidpointRounding.AwayFromZero</see> method.
+        /// This is the difference from <c>Math.Round</c>, where the default method is
+        /// <see cref="MidpointRounding">MidpointRounding.ToEven</see>.
         /// </summary>
-        /// <param name="d">A double-precision floating-point number to be rounded.</param>
-        /// <param name="mode">Specification for how to round value if it is midway between two other numbers.</param>
-        /// <returns>Rounded double number.</returns>
+        /// <param name="d">A decimal number to be rounded.</param>
+        /// <param name="decimals">The number of decimal places in the return value.</param>
+        /// <returns>Rounded decimal number.</returns>
+        public static double Round(this double d, int decimals)
+            => Math.Round(d, decimals, MidpointRounding.AwayFromZero);
+
+        /// <summary>
+        /// Rounds a decimal value to the nearest integer. A <paramref name="mode"/> specifies how to round
+        /// the value if it is midway between two numbers.
+        /// </summary>
+        /// <param name="d">A decimal number to be rounded.</param>
+        /// <param name="mode">Specification for how to round <paramref name="d"/> if it is midway between two other numbers.
+        /// </param>
+        /// <returns>Rounded decimal number.</returns>
         public static double Round(this double d, MidpointRounding mode)
             => Math.Round(d, mode);
+
+        /// <summary>
+        /// Rounds a decimal value to the nearest integer. A <paramref name="mode"/> specifies how to round
+        /// the value if it is midway between two numbers.
+        /// </summary>
+        /// <param name="d">A decimal number to be rounded.</param>
+        /// <param name="decimals">The number of decimal places in the return value.</param>
+        /// <param name="mode">Specification for how to round <paramref name="d"/> if it is midway between two other numbers.
+        /// </param>
+        /// <returns>Rounded decimal number.</returns>
+        public static double Round(this double d, int decimals, MidpointRounding mode)
+            => Math.Round(d, decimals, mode);
     }
 }
--- tests/Extensions/NumberExtensionsShould.cs
+++ tests/Extensions/NumberExtensionsShould.cs
@@ -1,217 +1,106 @@
 ﻿using FluentAssertions;
 using Kros.Extensions;
+using System;
 using Xunit;
 
 namespace Kros.Utils.UnitTests.Extensions
 {
     public class NumberExtensionsShould
     {
-        #region decimal
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.13)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDecimalToTwoPlace(decimal input, decimal expected)
-        {   
-            decimal actual = input.Round(2);            
-            actual.Should().Be(expected);            
+        [InlineData(10.105, 10.11)]
+        [InlineData(10.115, 10.12)]
+        [InlineData(10.125, 10.13)]
+        [InlineData(10.135, 10.14)]
+        [InlineData(10.145, 10.15)]
+        public void RoundDecimalToTwoPlaces(decimal input, decimal expected)
+        {
+            decimal actual = input.Round(2);
+            actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(10, 10)]
-        [InlineData(11.1, 11)]
-        [InlineData(11.2, 11)]
-        [InlineData(11.3, 11)]
-        [InlineData(11.4, 11)]
+        [InlineData(10.5, 11)]
         [InlineData(11.5, 12)]
-        [InlineData(11.6, 12)]
-        [InlineData(11.7, 12)]
-        [InlineData(11.8, 12)]
-        [InlineData(11.9, 12)]
-        public void ReturnRoundedDecimalTIintegralPart(decimal input, decimal expected)
+        [InlineData(12.5, 13)]
+        [InlineData(13.5, 14)]
+        [InlineData(14.5, 15)]
+        public void RoundDecimalToInteger(decimal input, decimal expected)
         {
             decimal actual = input.Round();
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.12)]
-        [InlineData(11.127, 11.12)]
-        [InlineData(11.128, 11.12)]
-        [InlineData(11.129, 11.12)]
-        public void ReturnRoundedDecimalToZeroTwoPlace(decimal input, decimal expected)
+        [InlineData(10.105, 10.10)]
+        [InlineData(10.115, 10.12)]
+        [InlineData(10.125, 10.12)]
+        [InlineData(10.135, 10.14)]
+        [InlineData(10.145, 10.14)]
+        public void RoundDecimalToTwoPlaces_MidpointRoundingToEven(decimal input, decimal expected)
         {
-            decimal actual = input.Round(2, System.MidpointRounding.ToZero);
+            decimal actual = input.Round(2, MidpointRounding.ToEven);
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDecimalToEvenTwoPlace(decimal input, decimal expected)
+        [InlineData(10.5, 10)]
+        [InlineData(11.5, 12)]
+        [InlineData(12.5, 12)]
+        [InlineData(13.5, 14)]
+        [InlineData(14.5, 14)]
+        public void RoundDecimalToInteger_MidpointRoundingToEven(decimal input, decimal expected)
         {
-            decimal actual = input.Round(2, System.MidpointRounding.ToEven);
+            decimal actual = input.Round(MidpointRounding.ToEven);
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.12)]
-        [InlineData(11.127, 11.12)]
-        [InlineData(11.128, 11.12)]
-        [InlineData(11.129, 11.12)]
-        public void ReturnRoundedDecimalToNegativeInfinityTwoPlace(decimal input, decimal expected)
-        {
-            decimal actual = input.Round(2, System.MidpointRounding.ToNegativeInfinity);
-            actual.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.13)]
-        [InlineData(11.122, 11.13)]
-        [InlineData(11.123, 11.13)]
-        [InlineData(11.124, 11.13)]
-        [InlineData(11.125, 11.13)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDecimalToPositiveInfinityTwoPlace(decimal input, decimal expected)
-        {
-            decimal actual = input.Round(2, System.MidpointRounding.ToPositiveInfinity);
-            actual.Should().Be(expected);
-        }
-        #endregion
-
-        #region double
-        [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.13)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDoubleToTwoPlace(double input, double expected)
+        [InlineData(10.105, 10.11)]
+        [InlineData(10.115, 10.12)]
+        [InlineData(10.125, 10.13)]
+        [InlineData(10.135, 10.14)]
+        [InlineData(10.145, 10.15)]
+        public void RoundDoubleToTwoPlaces(double input, double expected)
         {
             double actual = input.Round(2);
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(10, 10)]
-        [InlineData(11.1, 11)]
-        [InlineData(11.2, 11)]
-        [InlineData(11.3, 11)]
-        [InlineData(11.4, 11)]
+        [InlineData(10.5, 11)]
         [InlineData(11.5, 12)]
-        [InlineData(11.6, 12)]
-        [InlineData(11.7, 12)]
-        [InlineData(11.8, 12)]
-        [InlineData(11.9, 12)]
-        public void ReturnRoundedDoubleTIintegralPart(double input, double expected)
+        [InlineData(12.5, 13)]
+        [InlineData(13.5, 14)]
+        [InlineData(14.5, 15)]
+        public void RoundDoubleToInteger(double input, double expected)
         {
             double actual = input.Round();
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.12)]
-        [InlineData(11.127, 11.12)]
-        [InlineData(11.128, 11.12)]
-        [InlineData(11.129, 11.12)]
-        public void ReturnRoundedDoubleToZeroTwoPlace(double input, double expected)
+        [InlineData(10.105, 10.10)]
+        [InlineData(10.115, 10.12)]
+        [InlineData(10.125, 10.12)]
+        [InlineData(10.135, 10.14)]
+        [InlineData(10.145, 10.14)]
+        public void RoundDoubleToTwoPlaces_MidpointRoundingToEven(double input, double expected)
         {
-            double actual = input.Round(2, System.MidpointRounding.ToZero);
+            double actual = input.Round(2, MidpointRounding.ToEven);
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDoubleToEvenTwoPlace(double input, double expected)
+        [InlineData(10.5, 10)]
+        [InlineData(11.5, 12)]
+        [InlineData(12.5, 12)]
+        [InlineData(13.5, 14)]
+        [InlineData(14.5, 14)]
+        public void RoundDoubleToInteger_MidpointRoundingToEven(double input, double expected)
         {
-            double actual = input.Round(2, System.MidpointRounding.ToEven);
+            double actual = input.Round(MidpointRounding.ToEven);
             actual.Should().Be(expected);
         }
-
-        [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.12)]
-        [InlineData(11.122, 11.12)]
-        [InlineData(11.123, 11.12)]
-        [InlineData(11.124, 11.12)]
-        [InlineData(11.125, 11.12)]
-        [InlineData(11.126, 11.12)]
-        [InlineData(11.127, 11.12)]
-        [InlineData(11.128, 11.12)]
-        [InlineData(11.129, 11.12)]
-        public void ReturnRoundedDoubleToNegativeInfinityTwoPlace(double input, double expected)
-        {
-            double actual = input.Round(2, System.MidpointRounding.ToNegativeInfinity);
-            actual.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData(11.12, 11.12)]
-        [InlineData(11.121, 11.13)]
-        [InlineData(11.122, 11.13)]
-        [InlineData(11.123, 11.13)]
-        [InlineData(11.124, 11.13)]
-        [InlineData(11.125, 11.13)]
-        [InlineData(11.126, 11.13)]
-        [InlineData(11.127, 11.13)]
-        [InlineData(11.128, 11.13)]
-        [InlineData(11.129, 11.13)]
-        public void ReturnRoundedDoubleToPositiveInfinityTwoPlace(double input, double expected)
-        {
-            double actual = input.Round(2, System.MidpointRounding.ToPositiveInfinity);
-            actual.Should().Be(expected);
-        }
-        #endregion
     }
 }
