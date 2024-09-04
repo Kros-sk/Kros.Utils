9ef7fe6e202f56a2d68718107ff8f4802fe86185
Ján Slivka <slivka@kros.sk>
2024-09-04 15:13:03 +0200

    Update nuget packages and add support netstandard2.0.

---
+++ src/Extensions/CodeAnalysisExtensions.cs
@@ -0,0 +1,12 @@
+﻿#if IsNetStandard
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// Not null atribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class NotNullAttribute : Attribute
+    {
+    }
+}
+#endif
